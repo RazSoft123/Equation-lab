de355840ffe0449cefd2836f7f784afaf3dcd5fc
RazSoft <33057716+RazSoft123@users.noreply.github.com>
2022-06-15 08:43:26 +0530

    Takes user input.
    The panel is take user input in numerical form. It already shows the form of equation user just have to put numerical values in given boxes.

---
+++ src/ui/UserInputPanel.java
@@ -0,0 +1,14 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UserInputPanel extends JPanel {
+	
+	public UserInputPanel() {
+		setSize(new Dimension(640, 40));
+		setLocation(0,0);
+		setBackground(Color.RED);
+	}
+	
+}
